2741e59bf6fe8bb021fdfd09e000aeca38a7997b
Chotu Kumar <ck4001252@gmail.com>
2024-03-26 18:47:24 +0530

    file updated

--- index.js
+++ index.js
@@ -45,17 +45,35 @@ app.get('/api/v1/product', async (req, res) => {
 //Update product api
 
 app.put('/api/v1/product/:id', async (req, res) => {
-    let product = await Product.findById(req.params.id)
+    try {
+        let product = await Product.findById(req.params.id)
 
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        useFindAndModify: false,
-        runValidators: true
-    })
-    res.status(200).json({
-        success: true,
-        product
-    })
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            });
+        }
+
+        product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            useFindAndModify: false,
+            runValidators: true
+        })
+        res.status(200).json({
+            success: true,
+            message: "product updated successfully",
+            product
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "internal server error"
+        })
+
+    }
+
 })
 
 //Delete product api
